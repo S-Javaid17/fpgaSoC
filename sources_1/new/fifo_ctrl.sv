c188d5fd54399140ddb5d40f3ac1d56054a1eda4
Shoaib Javaid <17.shoaibj@gmail.com>
2024-07-29 17:42:37 -0400

    changed syntax --> "_succ = _logic + 1"

--- sources_1/new/fifo_ctrl.sv
+++ sources_1/new/fifo_ctrl.sv
@@ -21,8 +21,9 @@
 
 //doesn't deal with the data itself, just the flow of the data
 //basically a FSM
+
 module fifo_ctrl
-#(parameter ADDR_WIDTH = 3)
+#(parameter ADDR_WIDTH = 4)
 (
     input logic clk, reset,
     input logic wr, rd, //write and read commands
@@ -33,91 +34,83 @@ module fifo_ctrl
 
 //define the pointers as registers
 
-logic [ADDR_WIDTH - 1: 0] wr_ptr, wr_ptr_next;//write pointer, as a register, current and next state
-logic [ADDR_WIDTH - 1: 0] rd_ptr, rd_ptr_next;//read pointer, as a register 
+logic [ADDR_WIDTH-1:0] w_ptr_logic, w_ptr_next, w_ptr_succ;//write pointer, as a register, current and next state
+logic [ADDR_WIDTH-1:0] r_ptr_logic, r_ptr_next, r_ptr_succ;//read pointer, as a register 
 
 //Internal signals, to avoid dealing with the (external) full/empty flags directly
-logic full_next;
-logic empty_next;
+logic full_logic, empty_logic, full_next, empty_next;
 
 //State Register Logic
 
 always_ff @( posedge clk, posedge reset ) 
 begin
     if (reset)
-    begin
-        wr_ptr <= 0;
-        rd_ptr <= 0;
-        full <= 1'b0;
-        empty <= 1'b1; //will initially be empty
-    end   
+        begin
+            w_ptr_logic <= 0;
+            r_ptr_logic <= 0;
+            full_logic <= 1'b0;
+            empty_logic <= 1'b1; //will initially be empty
+        end   
     else
-    begin
-        wr_ptr <= wr_ptr_next;
-        rd_ptr <= rd_ptr_next;
-        full <= full_next;
-        empty <= empty_next;
-    end
+        begin
+            w_ptr_logic <= w_ptr_next;
+            r_ptr_logic <= r_ptr_next;
+            full_logic <= full_next;
+            empty_logic <= empty_next;
+        end
 end
 
 //Next State Logic
 
 always_comb 
 begin
-    //default values, will cover the lack of "else" statements
-    wr_ptr_next = wr_ptr;
-    rd_ptr_next = rd_ptr;
-    full_next = full;
-    empty_next = empty;
+    // successive pointer values
+      w_ptr_succ = w_ptr_logic + 1;
+      r_ptr_succ = r_ptr_logic + 1;
+      // default: keep old values
+      w_ptr_next = w_ptr_logic;
+      r_ptr_next = r_ptr_logic;
+      full_next = full_logic;
+      empty_next = empty_logic;
 
     unique case ({wr, rd})//the wr and rd signals control the behaviour of the controller
 
         2'b01: //read
             begin
-                if (~empty)
+                if (~empty_logic)
                 begin
-                    rd_ptr_next = rd_ptr + 1;
+                    r_ptr_next = r_ptr_succ;
                     full_next = 1'b0; //since the location, the rd pointer is at, was read from, it's no longer a full fifo
-                    if (rd_ptr_next == wr_ptr)// if the read pointer location is the same as the write pointer (aka it read the last value)
+                    if (r_ptr_succ == w_ptr_logic)// if the read pointer location is the same as the write pointer (aka it read the last value)
                         empty_next = 1'b1;//then the fifo is empty
                 end
             end
 
         2'b10: //write
             begin
-                if (~full)
+                if (~full_logic)//fifo not full
                 begin
-                    wr_ptr_next = wr_ptr + 1;
+                    w_ptr_next = w_ptr_succ;
                     empty_next = 1'b0; //since the location, the wr pointer is at, was written to, it's no longer an empty fifo
-                    if (wr_ptr_next == rd_ptr)// if the write pointer location is the same as the read pointer (aka it wrote the last available value)
+                     if (w_ptr_succ == r_ptr_logic)// if the write pointer location is the same as the read pointer (aka it wrote the last available value)
                         full_next = 1'b1;//then the fifo is full
                 end
             end
 
         2'b11: //read and write simultaneously --- possible, since it's a dual port ram
-            begin   
-                    //if it's empty, and you're reading/writing at the same time,---
-                    // --- you don't want the rd pointer to move up, you want it to ---
-                    //--- read from the same location that is being written to
-                if (empty)  
-                begin
-                    wr_ptr_next = wr_ptr;
-                    rd_ptr_next = rd_ptr;
-                end
-                else
-                begin
-                    wr_ptr_next = wr_ptr + 1;
-                    rd_ptr_next = rd_ptr + 1;
-                end    
+            begin
+               w_ptr_next = w_ptr_succ;
+               r_ptr_next = r_ptr_succ;
             end
-
         default: ;//2'b00 case is included here (neither read nor write)
     endcase
 end
 
 //Output Logic
 
-assign w_addr = wr_ptr;
-assign r_addr = rd_ptr;
+   assign w_addr = w_ptr_logic;
+   assign r_addr = r_ptr_logic;
+   assign full = full_logic;
+   assign empty = empty_logic;
 
 endmodule
