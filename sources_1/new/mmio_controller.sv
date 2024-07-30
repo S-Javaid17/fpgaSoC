250c58df0de884c4d27cff093dd277f9ff24e754
Shoaib Javaid <17.shoaibj@gmail.com>
2024-07-29 22:24:48 -0400

    Controller, Bridge, and I/O Map

---
+++ sources_1/new/mmio_controller.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/29/2024 10:12:28 PM
+// Design Name: 
+// Module Name: mmio_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mmio_controller(
+
+    );
+endmodule
